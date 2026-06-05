d9d8b7edde96156235c54479f155fd09ed5ff74a
gmpafc26 <gmorris@pafc.com.au>
2026-06-05 11:46:16 +0930

    Update config.js

--- config.js
+++ config.js
@@ -17,23 +17,26 @@ const LAUNCH_HUB_CONFIG = {
     RESOURCES:   "group_mm02ffen",
     PRODUCTS:    "group_mm02y11a",
     NOTES:       "group_mm40wwdj",
-    TASKS:       "group_mm41vv96"
+    TASKS:       "group_mm41vv96",
+    CONFIG:      "group_mm41n65p"
   },
+  CONFIG_TAG: "__config_department__",
   COLUMNS: {
-    NAME:            "name",
-    LAUNCH:          "text_mm405vsn",
-    LAUNCH_DATE:     "date_mm40n3fz",
-    LAUNCH_TIME:     "text_mm4027gn",
-    TIMELINE_DATE:   "date4",
-    OWNER:           "text_mm40pc5j",
-    NOTES:           "long_text_mm405hdn",
-    URL:             "link_mm40t1br",
-    BIGCOMMERCE_URL: "link_mm40z35t",
-    SEVERITY:        "color_mm403mv0",
-    TASK_STATUS:     "color_mm41skbm",
-    TASK_AREA:       "dropdown_mm41a0hx",
-    TASK_DUE:        "date_mm41ampt",
-    TASK_ASSIGNEE:   "multiple_person_mm41b66x",
-    TASK_DETAILS:    "long_text_mm418b1f"
+    NAME:             "name",
+    LAUNCH:           "text_mm405vsn",
+    LAUNCH_DATE:      "date_mm40n3fz",
+    LAUNCH_TIME:      "text_mm4027gn",
+    TIMELINE_DATE:    "date4",
+    OWNER:            "text_mm40pc5j",
+    NOTES:            "long_text_mm405hdn",
+    URL:              "link_mm40t1br",
+    BIGCOMMERCE_URL:  "link_mm40z35t",
+    SEVERITY:         "color_mm403mv0",
+    TASK_STATUS:      "color_mm41skbm",
+    TASK_DEPT:        "dropdown_mm41a0hx",
+    TASK_DUE:         "date_mm41ampt",
+    TASK_ASSIGNEE:    "multiple_person_mm41b66x",
+    TASK_DETAILS:     "long_text_mm418b1f",
+    TASK_COMPLETED:   "date_mm41wvp4"
   }
 };
